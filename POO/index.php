cda98b092e60ba5834ee728c31f8e6be98636358
Thiago <thiago.lacerda.fazzolo@hotmail.com>
2024-04-16 15:07:46 -0300

    Get, Set e constructor

--- POO/index.php
+++ POO/index.php
@@ -6,7 +6,7 @@
         private $peso;
 
 
-        function __construct($nome, $idade, $altura, $peso)
+        function __construct($nome=null, $idade=null, $altura=null, $peso=null)
         {
             $this->nome = $nome;
             $this->idade = $idade;
@@ -21,22 +21,22 @@
             $this->nome = $nome;
         }
         function getIdade(){
-            return $this->nome;
+            return $this->idade;
         }
-        function setIdade($nome){
-            $this->nome = $nome;
+        function setIdade($idade){
+            $this->idade = $idade;
         }
         function getAltura(){
-            return $this->nome;
+            return $this->altura;
         }
-        function setAltura($nome){
-            $this->nome = $nome;
+        function setAltura($altura){
+            $this->altura = $altura;
         }
         function getPeso(){
-            return $this->nome;
+            return $this->peso;
         }
-        function setPeso($nome){
-            $this->nome = $nome;
+        function setPeso($peso){
+            $this->peso = $peso;
         }
         
         function toString(){
@@ -45,15 +45,39 @@
     }
 
     $pessoa = new User('Thiago', 20, 1.86, 105);
+    $pessoa2 = new User();
 
 
     echo $pessoa->getNome() . "<br>";
-    echo "$pessoa->idade <br>";
-    echo "$pessoa->altura <br>";
-    echo "$pessoa->peso <br>";
+    echo $pessoa->getIdade() . "<br>";
+    echo $pessoa->getAltura() . "<br>";
+    echo $pessoa->getPeso() . "<br>";
     
     $pessoa->toString();
     
     $pessoa->setNome('João');
+    $pessoa->setIdade(32);
+    $pessoa->setAltura(1.65);
+    $pessoa->setPeso(73);
 
     echo $pessoa->getNome() . "<br>";
+    echo $pessoa->getIdade() . "<br>";
+    echo $pessoa->getAltura() . "<br>";
+    echo $pessoa->getPeso() . "<br>";
+
+    $pessoa->toString();
+
+    echo $pessoa2->getNome() . "<br>";
+    echo $pessoa2->getIdade() . "<br>";
+    echo $pessoa2->getAltura() . "<br>";
+    echo $pessoa2->getPeso() . "<br>";
+
+    $pessoa2->toString();
+    
+    $pessoa2->setNome('Thiago');
+    $pessoa2->setIdade(20);
+    $pessoa2->setAltura(1.86);
+    $pessoa2->setPeso(105);
+    
+    $pessoa2->toString();
+
